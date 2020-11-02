68b372c82e495ebf65e3799824f060371d3d3079
Tessakam <tessa.kam@hotmail.com>
2020-11-02 11:44:43 +0100

    add folder includes with header and footer

---
+++ View/class.php
@@ -0,0 +1,23 @@
+<?php require 'View/includes/header.php'?>
+
+<section class="container">
+    <h1 class="text-center mb-5">Class data</h1>
+
+    <div>
+    <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Location</th>
+            <th scope="col">Teacher ID</th>
+            <th scope="col"></th>
+            <th scope="col"></th>
+            <th scope="col"></th>
+        </tr>
+        </thead>
+    </div>
+
+</section>
+<?php require 'View/includes/footer.php'?>
+
