e3a2b7ca80c8b237ecca5cc94664e60bef0c5907
Tessakam <tessa.kam@hotmail.com>
2020-11-02 11:15:47 +0100

    added function insertData

--- Model/Classroom.php
+++ Model/Classroom.php
@@ -1,11 +1,11 @@
 <?php
 
 
-class Classroom
+class Classroom extends Database
 {
-private int $id;
-private string $name, $location;
-private int $teacher_id;
+    private int $id;
+    private string $name, $location;
+    private int $teacher_id;
 
     public function __construct(string $name, string $location)
     {
@@ -33,5 +33,13 @@ private int $teacher_id;
         return $this->teacher_id;
     }
 
+    public function insertData()
+    {
+        $handle = $this->openConnection()->prepare('INSERT INTO class (name, teacher_id, location) VALUES (:name, :teacher, :location)');
+        $handle->bindValue(':name', $this->getName());
+        $handle->bindValue(':teacher_id', $this->getTeacherId());
+        $handle->bindValue(':location', $this->getLocation());
+        $handle->execute();
+    }
 
 }
