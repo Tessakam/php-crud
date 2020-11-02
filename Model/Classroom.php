b5eb408c1a3553b1f2443d9573074edaa8035dc3
Tessakam <tessa.kam@hotmail.com>
2020-11-02 10:52:18 +0100

    Created Classroom class

--- Model/Classroom.php
+++ Model/Classroom.php
@@ -3,5 +3,35 @@
 
 class Classroom
 {
+private int $id;
+private string $name, $location;
+private int $teacher_id;
+
+    public function __construct(string $name, string $location)
+    {
+        $this->name = ucwords($name);
+        $this->location = ucwords($location);
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getLocation(): string
+    {
+        return $this->location;
+    }
+
+    public function getTeacherId(): int
+    {
+        return $this->teacher_id;
+    }
+
 
 }
