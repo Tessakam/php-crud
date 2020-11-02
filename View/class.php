a432f8b359774bf9d68da72e63eb9c07df896a41
Tessakam <tessa.kam@hotmail.com>
2020-11-02 12:34:41 +0100

    update View/class

--- View/class.php
+++ View/class.php
@@ -7,17 +7,29 @@
     <table class="table">
         <thead>
         <tr>
-            <th scope="col">#</th>
-            <th scope="col">Name</th>
+            <th scope="col">ID</th>
+            <th scope="col">Name class</th>
             <th scope="col">Location</th>
             <th scope="col">Teacher ID</th>
-            <th scope="col"></th>
-            <th scope="col"></th>
-            <th scope="col"></th>
+            <th scope="col">List of students</th>
+
         </tr>
+
+        <?php
+        foreach (//get allclasses as $class):
+            ?>
+            <tr>
+                <th><?php echo $class->getId(); ?></th>
+                <td><?php echo $class->getName(); ?></td>
+                <td><?php echo $class->getLocation(); ?></td>
+                <td><?php echo $class->getTeacherId(); ?></td>
+                <td><?php echo $class->getTeacherId(); ?></td>
+                </tr>
+        <?php endforeach; ?>
+
         </thead>
     </div>
-
 </section>
+
 <?php require 'View/includes/footer.php'?>
 
